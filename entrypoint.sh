a0fb3a83d1d3c888d824e7edaf906abbd4a54e95
Rafael Delboni <rafadelboni@gmail.com>
2021-12-13 11:51:49 -0300

    feat: Adds output-type argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rules_repository="$1"
+output_type="$2"
 
 cd /github/workspace/
 clj-holmes fetch-rules -r "$rules_repository"
-clj-holmes scan -p . -t sarif -o clj-holmes.sarif 
+clj-holmes scan -p . -t "$output_type" -o clj-holmes.sarif
