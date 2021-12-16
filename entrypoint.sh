690909b2fab328ecc92eaf295e97efbc8e0157ea
Rafael Delboni <rafadelboni@gmail.com>
2021-12-16 12:04:59 -0300

    refact: Better naming and ifs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,14 @@
 
 rules_repository="$1"
 output_type="$2"
-if [ $3 == "true" ]; then fail_on_result="--fail-on-result"; else fail_on_result="--no-fail-on-result"; fi
+fail_on_result="$3"
+
+if [[ $fail_on_result == "true" ]]; then
+  fail_on_result_cmd="--fail-on-result";
+else
+  fail_on_result_cmd="--no-fail-on-result";
+fi
 
 cd /github/workspace/
 clj-holmes fetch-rules -r "$rules_repository"
-clj-holmes scan -p . $fail_on_result -t "$output_type" -o clj-holmes.sarif
+clj-holmes scan -p . $fail_on_result_cmd -t "$output_type" -o clj-holmes.sarif
