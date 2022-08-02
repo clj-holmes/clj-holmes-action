92d59ed56e48c0e9ffd7ae39bf700b1b1959cc11
rancorzinho <9276791+rancorzinho@users.noreply.github.com>
2022-08-02 17:25:16 -0300

    Add --no-verbose as a default flag (#6)
    * add no verbose input
    
    * change no-verbose to verbose
    
    Co-authored-by: Wikiz <wikiz@wikiz.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ output_type="$2"
 output_file="$3"
 fail_on_result="$4"
 
+if [[ $verbose == "true" ]]; then
+    verbose_cmd="--verbose"
+else
+    verbose_cmd="--no-verbose"
+fi
+
 if [[ $fail_on_result == "true" ]]; then
   fail_on_result_cmd="--fail-on-result";
 else
@@ -19,4 +25,4 @@ fi
 
 cd /github/workspace/
 clj-holmes fetch-rules -r "$rules_repository"
-clj-holmes scan -p . $fail_on_result_cmd $output_cmd
+clj-holmes scan -p . $fail_on_result_cmd $verbose_cmd $output_cmd
