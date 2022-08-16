f1a86808f93844ec0fa1b6aa92c523b868fbbf41
rancorzinho <9276791+rancorzinho@users.noreply.github.com>
2022-08-16 15:24:12 -0300

    Add support for working directory (#7)
    * add support for working directory
    
    * working-directory -> path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ rules_repository="$1"
 output_type="$2"
 output_file="$3"
 fail_on_result="$4"
+path="$5"
+
+if [[ -d ${working_directory} ]]; then
+  path="/github/workspace/${path}"
+else
+  path="/github/workspace/"
+fi
 
 if [[ $verbose == "true" ]]; then
     verbose_cmd="--verbose"
@@ -23,6 +30,6 @@ else
   output_cmd="-t $output_type -o $output_file";
 fi
 
-cd /github/workspace/
+cd ${path}
 clj-holmes fetch-rules -r "$rules_repository"
 clj-holmes scan -p . $fail_on_result_cmd $verbose_cmd $output_cmd
